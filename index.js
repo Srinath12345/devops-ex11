e443ea61b0b1eda97e0f683bcea553ab9d20948a
harikrish-s <harikrishnan21110422@snuchennai.edu.in>
2024-10-27 22:12:52 +0530

    updated app with time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! Updated at: Sun Oct 27 22:12:34 IST 2024");
 });
 
 app.listen(port, () => {
