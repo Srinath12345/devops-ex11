ab2be95240aee59c5e018ec1d0b0effc1228b8ad
Srinath12345 <75529369+Srinath12345@users.noreply.github.com>
2024-11-06 11:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("in master Updated at: Sun Oct 27 22:12:34 IST 2024");
+  res.send("in master Updated at:today");
 });
 
 app.listen(port, () => {
